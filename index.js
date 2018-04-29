936532bb52f43d6acd160cda1b62a143afc9c2af
moshe malka <moshemalka2014@gmail.com>
2018-04-30 00:48:11 +0300

    Update index.js
    adding comments and 'toTitleCase' function

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ exports.handler = (event, context, callback) => {
 
   const API_KEY = event.api_key;
   const city_name = event.city_name;
-  let cityCode = IsraeliCities.filter(item => item.name === city_name)[0].id;
+  // convert city name to Title Case - that is the naming convention in the cities file.
+  const toTitleCase = (str) => str.replace(/\w\S*/g, txt => {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  // grab the object containing the city name and get it's id (city code).
+  let cityCode = IsraeliCities.filter(item => item.name === toTitleCase(city_name))[0].id;
 
   if(!cityCode || !city_name || ! API_KEY) callback("Error");
+  
   let URL = `http://api.openweathermap.org/data/2.5/weather?id=${city_code}&appid=${API_KEY}&units=metric`;
 
-
+  // call api with api key and city code.
   axios.get(URL)
       .then(res => {
         if(res.status !== 200) callback("Error");
         else{
+          // building json response according to the model that was specified.
           let json_res = {
             "location": {
               "name": res.data.name,
